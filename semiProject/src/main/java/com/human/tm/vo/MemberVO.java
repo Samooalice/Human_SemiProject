d099a4580af5e3bc3d82ba8bfa28d5b38e17f4d7
human <human@DESKTOP-R2148PJ>
2024-05-24 20:16:42 +0900

    진짜 VO 수정

--- semiProject/src/main/java/com/human/tm/vo/MemberVO.java
+++ semiProject/src/main/java/com/human/tm/vo/MemberVO.java
@@ -1,8 +1,12 @@
 package com.human.tm.vo;
 
+import java.sql.Clob;
+
 public class MemberVO {
-	private int product_no;
-	private String birth, main_bank, interest_type, product_type, product_bank, product_name;
+	private int product_no, mno;
+	private String birth, main_bank, interest_type, product_type, product_bank, product_name, nickname, period_day, isshow, join_target;
+	private Clob product_file;
+	
 	
 	public int getProduct_no() {
 		return product_no;
@@ -10,23 +14,11 @@ public class MemberVO {
 	public void setProduct_no(int product_no) {
 		this.product_no = product_no;
 	}
-	public String getProduct_type() {
-		return product_type;
+	public int getMno() {
+		return mno;
 	}
-	public void setProduct_type(String product_type) {
-		this.product_type = product_type;
-	}
-	public String getProduct_bank() {
-		return product_bank;
-	}
-	public void setProduct_bank(String product_bank) {
-		this.product_bank = product_bank;
-	}
-	public String getProduct_name() {
-		return product_name;
-	}
-	public void setProduct_name(String product_name) {
-		this.product_name = product_name;
+	public void setMno(int mno) {
+		this.mno = mno;
 	}
 	public String getBirth() {
 		return birth;
@@ -46,11 +38,63 @@ public class MemberVO {
 	public void setInterest_type(String interest_type) {
 		this.interest_type = interest_type;
 	}
-	
+	public String getProduct_type() {
+		return product_type;
+	}
+	public void setProduct_type(String product_type) {
+		this.product_type = product_type;
+	}
+	public String getProduct_bank() {
+		return product_bank;
+	}
+	public void setProduct_bank(String product_bank) {
+		this.product_bank = product_bank;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getPeriod_day() {
+		return period_day;
+	}
+	public void setPeriod_day(String period_day) {
+		this.period_day = period_day;
+	}
+	public String getIsshow() {
+		return isshow;
+	}
+	public void setIsshow(String isshow) {
+		this.isshow = isshow;
+	}
+	public String getJoin_target() {
+		return join_target;
+	}
+	public void setJoin_target(String join_target) {
+		this.join_target = join_target;
+	}
+	public Clob getProduct_file() {
+		return product_file;
+	}
+	public void setProduct_file(Clob product_file) {
+		this.product_file = product_file;
+	}
 	@Override
 	public String toString() {
-		return "MemberVO [birth=" + birth + ", main_bank=" + main_bank + ", interest_type=" + interest_type + "]";
+		return "MemberVO [product_no=" + product_no + ", mno=" + mno + ", birth=" + birth + ", main_bank=" + main_bank
+				+ ", interest_type=" + interest_type + ", product_type=" + product_type + ", product_bank="
+				+ product_bank + ", product_name=" + product_name + ", nickname=" + nickname + ", period_day="
+				+ period_day + ", isshow=" + isshow + ", join_target=" + join_target + ", product_file=" + product_file
+				+ "]";
 	}
+	
 	
 
 	
